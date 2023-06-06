a1c3ef58d89dd802b84e414ef8005bfade8e9fa5
Yamini Rathish <yamini.rathish@gmail.com>
2023-06-06 18:06:48 +0200

    change on click

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  function changeOnReload(){
+    
+      const change = ["Good","Wonderful","Great","Awesome"];
+      const int = Math.floor(Math.random() *4);
+      return(change[int])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    Hi Welcome 
+    <p>Have a {changeOnReload()} day </p>
+   </div>
+    
   );
 }
 
